fbee20b311ddb94249d02efb1f454942759608df
Darin-B <95306379+Darin-B@users.noreply.github.com>
2022-04-05 11:43:06 -0400

    Add files via upload

---
+++ Adding machine.py
@@ -0,0 +1,4 @@
+first_num = int(input('What is the first number you wanr to add?'))
+second_num = int(input('What is the second number you wanr to add?'))
+sum = first_num + second_num
+print("The sum of your two numbers is", sum)
